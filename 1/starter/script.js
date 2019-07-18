989491de81f75fd5288339650acf5c5de94125d5
Sarmad Rezayat <sarmad.re@gmail.com>
2019-07-18 16:00:15 +0200

    first task acompilished

--- 1/starter/script.js
+++ 1/starter/script.js
@@ -1,1 +1,13 @@
 console.log("Hello Word!!!");
+
+class personBpi {
+  constructor(name, a, b) {
+    this.name = name;
+    this.a = a;
+    this.b = b;
+  }
+  majorBpi() {
+    const bpi = this.a / Math.pow(this.b, 2);
+    console.log(`Dear ${this.name} your BPI is ${bpi}`);
+  }
+}
